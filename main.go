33da206a39d1758bfec9fc821c0bbc90a12bfc5f
Daniel Grzelak <daniel.grzelak@plerion.com>
2024-02-14 14:51:44 +1100

    new version

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	
-	"terraform-provier-terrarizer/internal/provider"
+	"terraform-provider-terrarizer/internal/provider"
 )
 
 // Run "go generate" to format example terraform files and generate the docs for the registry/website
